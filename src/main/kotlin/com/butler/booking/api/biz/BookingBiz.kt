5c28f801e89385f483ca1f6d3e3a48d07e0acb55
RyuNaGyoung <fsraba2@gmail.com>
2024-08-11 17:39:20 +0900

    기능 수정

--- src/main/kotlin/com/butler/booking/api/biz/BookingBiz.kt
+++ src/main/kotlin/com/butler/booking/api/biz/BookingBiz.kt
@@ -2,7 +2,9 @@ package com.butler.booking.api.biz
 
 import com.butler.booking.api.ui.vo.BookingVO
 import com.butler.booking.api.ui.vo.ResVO
+import com.butler.booking.model.dto.MemberDTO
 
 interface BookingBiz {
     fun booking (bookingVO: BookingVO) : ResVO
+    fun list (memberDTO: MemberDTO) : ResVO
 }
